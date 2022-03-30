509c9ee9479dde9a969f00d55860e398c6045ec8
Ulukmyrza27 <uluk.osmonov@gmail.com>
2022-03-30 16:13:47 +0600

    context added

--- src/Routing.js
+++ src/Routing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 
 const Routing = () => {
@@ -7,6 +7,9 @@ const Routing = () => {
     <div>
       <BrowserRouter>
         <Header />
+        <Routes>
+          <Route />
+        </Routes>
       </BrowserRouter>
     </div>
   );
