8ff767de4cad93c059c747053136545243241349
Ulukmyrza27 <uluk.osmonov@gmail.com>
2022-03-31 17:42:23 +0600

    CarouselPic, deleteCRUD

--- src/Routing.js
+++ src/Routing.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddGun from "./components/AddGun/AddGun";
-import EditGun from "./components/EditGun/EditGun";
 import Header from "./components/Header/Header";
+import ImageStart from "./components/ImageStart/ImageStart";
+import NavBar from "./components/NavBar/NavBar";
 import SteamList from "./components/SteamList/SteamList";
+import CarouselPics from "./components/CarouselPics/CarouselPics";
+import About from "./components/About/About";
+import HomePage from "./components/HomePage/HomePage";
 
 const Routing = () => {
   return (
     <div>
-      <Header />
       <BrowserRouter>
+        <NavBar />
         <Routes>
+          <Route exact path="/" element={<HomePage />} />
           <Route path="/list" element={<SteamList />} />
           <Route path="/add" element={<AddGun />} />
-          {/* <Route path="/edit" element={<EditGun />} /> */}
+          <Route path="/about" element={<About />} />
         </Routes>
+        <ImageStart />
+        <CarouselPics />
       </BrowserRouter>
     </div>
   );
